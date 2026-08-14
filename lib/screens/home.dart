849767826d710932254dfd204de6b36d5dae9ec2
Cristóbal Salinas <contactol.csalinas@gmail.com>
2026-08-13 23:47:18 -0400

    :art: Agregando interactividad a la aplicacion

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:knucklebones/widgets/dice_board.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<List<int?>> firstPlayerBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+  List<List<int?>> secondPlayerBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+
+  void changePlayerBoardValue(int player, int row, int column, int? value) {
+    // Actualizo el valor de el tablero del jugador correspondiente
+    setState((){
+      if (player == 1) {
+        firstPlayerBoard[row][column] = value;
+      } else if (player == 2) {
+        secondPlayerBoard[row][column] = value;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
